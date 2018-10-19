9cf6deeca557df19a16dacbbb096bf3b75fa076e
0gjimenez <42749723+0gjimenez@users.noreply.github.com>
2018-10-19 13:56:53 -0500

    Add files via upload

--- loops.js
+++ loops.js
@@ -1,35 +1,34 @@
 // For-loop functions
 
 function positiveIntSum(limit){
-let sum = 0;
-for(let i = 1; i <= limit; i++)
-sum = sum + i;
-
+    let sum = 0;
+    for(let i = 1; i <= limit; i++)
+        sum = sum + i;
 }
-
-
+    return sum
+}
 function additionPatternSum(start, diff, limit){
-let sum = start;
-for(let k = 0; k <= limit; k++){
-sum = sum + diff * k; 
+    let sum = start;
+    for(let k = 0; k <= limit; k++){
+        sum = sum + diff * k; 
 }
-
-
+    return sum
+}
 
 
 // While-loop function
 
 function multiplicationPatternSum(start, factor, max){
-let sum = start;
-let j = 2; 
-let n = max;
-let factor = 8;
-while(j <= n){
-sum = sum + j - factor * 3; 
+    let sum = start;
+    let j = start; 
+    while(j <= max){
+        sum = sum + j * factor;
+        i = i * factor 
+}
+    return sum
 }
 
 
 
-
-
-//Later copy paste the checking code below here.
+//Later copy paste the checking code below here
+consol.log("q1:"+positiveIntSum(1500));
